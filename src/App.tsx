028ad314707c419ef3a0a655dea4cdee699e9485
guosheng <guosheng@mchz.com.cn>
2021-09-01 10:00:47 +0800

    feat：禁用按钮文字提示

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 // import { Button } from 'antd';
-import EditTableView from './module/EditTable';
+// import EditTableView from './module/EditTable';
+import DisabledButton from './module/Button/DisabledButton';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <EditTableView />
+        <DisabledButton disabledText="测试语句"/>
       </div>
     );
   }
