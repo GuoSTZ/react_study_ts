28b1c159dd43d568b313054eb663a1d004a70803
guosheng <guosheng@mchz.com.cn>
2021-09-27 09:03:40 +0800

    feat: add notes

--- src/module/Progress/index.tsx
+++ src/module/Progress/index.tsx
@@ -12,7 +12,13 @@ export interface ProgressViewProps extends ProgressProps {
    * 统一控制分隔符的显示和隐藏
    */
   colon?: boolean;
+  /**
+   * label 标签布局，同 <Col> 组件，设置 span offset 值，如 {span: 3, offset: 12} 或 sm: {span: 3, offset: 12}
+   */
   labelCol?: any;
+  /**
+   * 需要为输入控件设置布局样式时，使用该属性，用法同 labelCol
+   */
   wrapperCol?: any;
 }
 
