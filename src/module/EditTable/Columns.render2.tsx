ad698140cb9f4195de32f13fbcbd7d9702611a44
guosheng <guosheng@mchz.com.cn>
2022-01-26 15:52:34 +0800

    feat: 可编辑表格调整

--- src/module/EditTable/Columns.render2.tsx
+++ src/module/EditTable/Columns.render2.tsx
@@ -19,7 +19,7 @@ export function renderConfig(options: any, props: any) {
       title: 'account',
       key: 'account',
       dataIndex: 'account',
-      width: '50%',
+      width: '40%',
       renderCol: (text: string, row: any, instance: Object) => text,
       editConfig: (data: any, editingKey: any, form: any) => ({
         rules: [
@@ -51,9 +51,10 @@ export function renderConfig(options: any, props: any) {
       title: 'number',
       key: 'number',
       dataIndex: 'number',
-      width: '40%',
+      width: '30%',
       renderCol: (text: string, row: any, instance: Object) => text,
       editConfig: (data: any, editingKey: any, form: any) => ({
+        initialValue: 12,
         rules: [
           {
             validator: (rule: any, value: any, callback: any) => {
@@ -68,6 +69,30 @@ export function renderConfig(options: any, props: any) {
       editComponent: (text: any, row: any, instance: Object, form: any, data: any) => {
         return (
           <Input placeholder='请输入' style={{width:"100%"}}/>
+        )
+      }
+    },
+    {
+      title: 'number2',
+      key: 'number2',
+      dataIndex: 'number2',
+      width: '20%',
+      renderCol: (text: string, row: any, instance: Object) => text,
+      editConfig: (data: any, editingKey: any, form: any) => ({
+        rules: [
+          {
+            validator: (rule: any, value: any, callback: any) => {
+              if(!value) {
+                return callback("请输入内容2222");
+              }
+              return callback();
+            }
+          }
+        ]
+      }),
+      editComponent: (text: any, row: any, instance: Object, form: any, data: any) => {
+        return (
+          <Input placeholder='请输入2222' style={{width:"100%"}}/>
         )
       }
     }
--- src/module/EditTable/check.tsx
+++ src/module/EditTable/check.tsx
@@ -3,7 +3,7 @@ import Form from 'antd/es/form';
 import EditTable from './components/error_EditTable';
 import { WrapperRef } from '@mcfed/components';
 import { renderConfig } from './Columns.render2';
-import { Checkbox, Button, Input } from 'antd';
+import { Checkbox, Button, Input, Radio } from 'antd';
 
 class CheckWrap extends React.Component<any, any> {
   nodeRef = React.createRef();
@@ -18,7 +18,7 @@ class CheckWrap extends React.Component<any, any> {
   };
   render(): React.ReactNode {
     const { getFieldDecorator } = this.props.form;
-    let mode: any = 'full';
+    let mode: any = 'row';
     let tableConf = {
       rowKey: 'key',
       data: [],
@@ -32,6 +32,7 @@ class CheckWrap extends React.Component<any, any> {
       bordered: false,
       maxNum: 4,
       maxErrorMsg: "最多可添加4个账号",
+      hideCancelConfirm: false,
       // hideDeleteConfirm: true
     };
     return (
@@ -41,7 +42,13 @@ class CheckWrap extends React.Component<any, any> {
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 20 }}>
           {getFieldDecorator('edit', {
-            rules: [{ required: true, message: '请填选内容' }],
+            rules: [
+              { required: true, message: '请填选内容' },
+              { validator: (rule: any, value: any, callback: any) => {
+                console.log(rule, value);
+                callback();
+              } }
+            ],
           })(
             <WrapperRef>
               <EditTable
@@ -51,14 +58,14 @@ class CheckWrap extends React.Component<any, any> {
             </WrapperRef>
           )}
         </Form.Item>
-        <Form.Item
+        {/* <Form.Item
           label={"输入框"}
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 20 }}>
           {getFieldDecorator('input', {
             rules: [{ required: true, message: '请输入' }],
           })(
-            <Input placeholder="请输入内容"/>
+            <Input placeholder="请输入内容" />
           )}
         </Form.Item>
         <Form.Item
@@ -71,6 +78,21 @@ class CheckWrap extends React.Component<any, any> {
             <Checkbox>xxxx</Checkbox>
           )}
         </Form.Item>
+        <Form.Item
+          label={"单选框"}
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 20 }}>
+          {getFieldDecorator('radio', {
+            rules: [{ required: true, message: '请勾选' }],
+          })(
+            <Radio.Group>
+              <Radio value={1}>A</Radio>
+              <Radio value={2}>B</Radio>
+              <Radio value={3}>C</Radio>
+              <Radio value={4}>D</Radio>
+            </Radio.Group>
+          )}
+        </Form.Item> */}
         <Form.Item
           wrapperCol={{ span: 20, offset: 4 }}>
           <Button type="primary" htmlType="submit" className="login-form-button">
