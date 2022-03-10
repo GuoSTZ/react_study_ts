ea4f2b75d2c8f900ef7e8d5be3e7edf642babb62
guosheng <guosheng@mchz.com.cn>
2022-03-10 10:39:38 +0800

    feat: 空数据disabled处理

--- src/module/Select/components/DropdownRender_class.tsx
+++ src/module/Select/components/DropdownRender_class.tsx
@@ -49,16 +49,18 @@ export default class DropdownRender_class extends React.Component<DropdownRender
     menuNode?.props?.menuItems
       .slice(start, end)
       .map((item: any, idx: number) => {
+        let isDisabledItem = false;
         const index = start + idx;
         const style = this.handleItemStyle(index);
         // 为空时
         if (item.key === "NOT_FOUND") {
+          isDisabledItem = true;
           delete style.height;
         }
         new_menuItems.push(
           React.cloneElement(item, {
             style: { ...item.style, ...style },
-            disabled: isCheckAll,
+            disabled: isCheckAll || isDisabledItem,
           })
         )
       });
